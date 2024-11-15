48b61954f8ef6e21dc4a7fb839a04a9c9627bb1a
Max Dupley <51045274+QuadDarv1ne@users.noreply.github.com>
2024-11-15 12:20:55 +0300

    Update dbconnect.py

--- app/database/dbconnect.py
+++ app/database/dbconnect.py
@@ -2,11 +2,16 @@ from sqlalchemy import func
 from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
+from dotenv import load_dotenv
+import os
 
-database_url = 'mysql+aiomysql://root:password@localhost/sportsclub'
+# Загрузка переменных окружения
+load_dotenv()
+
+# Получение URL базы данных из переменной окружения
+database_url = os.getenv("DATABASE_URL", "mysql+aiomysql://root:password@localhost/sportsclub")
 engine = create_async_engine(url=database_url)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
-
 
 class Base(AsyncAttrs, DeclarativeBase):
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
