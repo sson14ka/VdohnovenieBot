f8936a9bb9700a30242012e840291452ebe473f9
Max Dupley <51045274+QuadDarv1ne@users.noreply.github.com>
2024-11-15 12:22:08 +0300

    Update dbconnect.py

--- app/database/dbconnect.py
+++ app/database/dbconnect.py
@@ -4,15 +4,30 @@ from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
 from dotenv import load_dotenv
 import os
+import logging
+
+# Настройка логирования
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 # Загрузка переменных окружения
 load_dotenv()
 
-# Получение URL базы данных из переменной окружения
+# Конфигурация базы данных
 database_url = os.getenv("DATABASE_URL", "mysql+aiomysql://root:password@localhost/sportsclub")
 engine = create_async_engine(url=database_url)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
 
+# Базовый класс для моделей
 class Base(AsyncAttrs, DeclarativeBase):
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+
+# Функция для корректного завершения работы
+async def shutdown():
+    await engine.dispose()
+
+# Генератор сессий
+async def get_session() -> AsyncSession:
+    async with async_session_maker() as session:
+        yield session
